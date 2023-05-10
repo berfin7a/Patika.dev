212169786885e82b9c6329901e3a8b0715999e2c
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-05-10 18:28:00 +0300

    Create KDVHesaplama.java

---
+++ KDVHesaplama.java
@@ -0,0 +1,21 @@
+package java101;
+
+import java.util.Scanner ;
+public class kdv {
+    public static void main (String[]args) {
+        double fiyat ;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("lütfen fiyat bilgisi giriniz :");
+        fiyat = inp.nextDouble();
+        double kdv=fiyat*0.18;
+        double kdv1=fiyat*0.08;
+        System.out.println("kdvsiz tutar : " + fiyat);
+        double sonuç =(fiyat>=0 && fiyat<=1000) ?  kdv : kdv1 ;
+        double toplam=sonuç+fiyat;
+
+
+        System.out.println("kdv tutarı : " +sonuç );
+        System.out.println("KDV'li Tutar: "+ toplam);
+
+    }
+}
