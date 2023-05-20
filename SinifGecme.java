92fa393b8d956fc34de2b41ac866b48650a6afc0
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-05-20 11:32:42 +0300

    Create SinifGecme.java

---
+++ SinifGecme.java
@@ -0,0 +1,80 @@
+package java101;
+import java.util.Scanner ;
+
+public class SinifGecme {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int mat, fizik, turkce, kimya, muzik;
+        double toplam =0, average, derssayisi =5;
+
+        System.out.println("matematik notunuzu giriniz  : ");
+        mat = input.nextInt();
+        System.out.println("fizik notunuzu girniz :");
+        fizik = input.nextInt();
+        System.out.println("turkce notunuzu giriniz  : ");
+        turkce = input.nextInt();
+        System.out.println("kimya notunuzu giriniz  : ");
+        kimya = input.nextInt();
+        System.out.println("muzik notunuzu giriniz  : ");
+        muzik = input.nextInt();
+
+        if (mat <= 100 && mat >= 0){
+        toplam += mat;
+
+        }else{
+            System.out.println("notunuz 0 ile 100 arasında olmalıdır");
+            mat=0;
+            --derssayisi;
+        }
+        if (fizik<= 100 && fizik >= 0){
+            toplam += fizik;
+            ++derssayisi;
+        }else{
+            System.out.println("notunuz 0 ile 100 arasında olmalıdır");
+            fizik=0;
+            --derssayisi;
+        }
+
+        if (turkce <= 100 && turkce >= 0){
+            toplam += turkce;
+            ++derssayisi;
+        }else{
+            System.out.println("notunuz 0 ile 100 arasında olmalıdır");
+            turkce=0;
+            --derssayisi;
+        }
+
+        if (kimya <= 100 && kimya >= 0){
+            toplam += kimya;
+            ++derssayisi;
+        }else{
+            System.out.println("notunuz 0 ile 100 arasında olmalıdır");
+            kimya=0;
+            --derssayisi;
+        }
+
+        if (muzik <= 100 && muzik >= 0){
+            toplam += muzik;
+            ++derssayisi;
+        }else{
+            System.out.println("notunuz 0 ile 100 arasında olmalıdır");
+            muzik=0;
+            --derssayisi;
+        }
+
+        average = toplam/derssayisi ;
+
+
+        if(average < 55) {
+            System.out.println("sınıfı geçemediniz ");
+        }else{
+            System.out.println("sınıfı geçtiniz ");
+        }
+        System.out.println("ortalamanız :" + average);
+
+
+
+
+    }
+}
