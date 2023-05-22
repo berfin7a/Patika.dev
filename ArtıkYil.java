0b3657a663ca93e72f9b2bbd0ebc17cd65cf8ed1
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-05-23 02:08:10 +0300

    Create ArtıkYil.java

---
+++ ArtıkYil.java
@@ -0,0 +1,31 @@
+package java101;
+import java.util.Scanner;
+
+public class ArtilYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int yil;
+
+        System.out.println("yıl : ");
+        yil = input.nextInt();
+
+
+        if (yil % 100 == 0) {
+            if (yil % 400 == 0) {
+                System.out.print(yil);
+                System.out.println(" bir artık yıldır");
+            }else{
+                System.out.print(yil);
+                System.out.println(" bir artık yıl değildir");
+            }
+
+        }else if (yil%4 == 0){
+                System.out.print(yil);
+                System.out.println(" bir artık yıldır");
+         }else {
+                System.out.print(yil);
+                System.out.println(" bir artık yıl değildir");
+            }
+
+        }
+    }
