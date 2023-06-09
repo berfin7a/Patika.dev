b214e55409ed73b562ae584fc266d2a564896d09
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-06-09 17:51:03 +0300

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package java101;
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int result =0, n1=0, n2=1;
+
+        for(int i=0 ;i <=9; i++){System.out.println(n1 + " ");
+           result = n1+n2;
+           n1 = n2;
+           n2 = result;
+
+
+
+
+        }
+
+    }
+}
