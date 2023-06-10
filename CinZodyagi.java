909cec28b95a80c9a1a9f2f2ba5367a541b4b3be
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-06-10 12:49:31 +0300

    Update and rename CinZodyagı.java to CinZodyagi.java

--- CinZodyagi.java
+++ CinZodyagi.java
@@ -1,7 +1,7 @@
 package java101;
 import java.util.Scanner;
 
-public class cinZodyagi {
+public class CinZodyagi {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int yil, mod ;
