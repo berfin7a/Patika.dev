83b2e27ab50fa758a1eb591aba9fdc6b5d06e21f
berfin7a <114851768+berfin7a@users.noreply.github.com>
2023-06-29 22:33:59 +0300

    Update RecursivePower.java

--- RecursivePower.java
+++ RecursivePower.java
@@ -1,25 +1,18 @@
 package java101;
 
 import java.util.Scanner;
-
-public class RecursivePower {
+public class RecursivePower{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(power(5,3));
+    }
     static int power(int a, int b){
         int result =1;
-        for (int i = 1;i<=b;i++){
-            result *= a;
+        if (b == 0) {
+            return result;
         }
+        result *= a*power(a,--b);
         return result;
-
     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("taban sayısı :");
-        int a = input.nextInt();
-        System.out.println("üs değeri :");
-        int b = input.nextInt();
-
-        System.out.println("sonuç : " + power(a,b));
-    }
 }
